33f8a0f5f06d1ea567ef9afad3930774b2560a0b
Jeffrey Wan <soulitzer@gmail.com>
2018-09-04 18:48:47 -0400

    Done

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
-
+@app.route('/pokemon/<poke_id>', methods=['GET'])
+def get_pokemon_by_id(poke_id):
+    r = requests.get('http://pokeapi.co/api/v2/pokemon/{}'.format(poke_id))
+    poke_name = r.json()['name']
+    return render_template("index.html",poke_id=poke_id, poke_name=poke_name)
 
 if __name__ == '__main__':
     app.run()
